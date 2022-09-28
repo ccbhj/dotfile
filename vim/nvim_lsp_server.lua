047d34a58def42acf27b4acba930851f2dd043a6
Bingjia Chen <bingjia.chen@seamoney.com>
2022-09-28 15:06:34 +0800

    upgrade theme and lsp_server

--- vim/nvim_lsp_server.lua
+++ vim/nvim_lsp_server.lua
@@ -169,3 +169,22 @@ lsp_installer.on_server_ready(function(server)
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
 end)
+
+local util = require('lspconfig/util')
+local lastRootPath = nil
+local gopath = os.getenv("GOPATH")
+if gopath == nil then
+  gopath = ""
+end
+local gopathmod = gopath..'/pkg/mod'
+
+require('lspconfig')['gopls'].setup{
+  root_dir = function(fname)
+    local fullpath = vim.fn.expand(fname, ':p')
+    if string.find(fullpath, gopathmod) and lastRootPath ~= nil then
+        return lastRootPath
+    end
+    lastRootPath = util.root_pattern("go.mod", ".git")(fname)
+    return lastRootPath
+  end,
+}
