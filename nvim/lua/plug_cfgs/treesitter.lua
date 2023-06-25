c061eaeea209c7c84bcfecfcdb22b16644fe44ec
Bingjia Chen <bingjia.chen@seamoney.com>
2023-06-25 12:19:10 +0800

    fix nvim slow movement

--- nvim/lua/plug_cfgs/treesitter.lua
+++ nvim/lua/plug_cfgs/treesitter.lua
@@ -112,12 +112,4 @@ require "nvim-treesitter.configs".setup {
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   },
-  refactor = {
---     highlight_current_scope = { enable = true },
-    highlight_definitions = {
-      enable = true,
-      -- Set to false if you have an `updatetime` of ~100.
-      clear_on_cursor_move = true,
-    },
-  },
 }
